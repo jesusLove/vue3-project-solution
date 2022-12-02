67a6f07276b61ba93e1563a8ab5c7f5e4bf415ff
李千千 <jesus_lqq@163.com>
2022-12-02 14:20:18 +0800

    feat: 路由表

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,19 @@ const publicRoutes = [
   },
   {
     path: '/',
-    component: () => import('@/layout/index.vue')
+    component: layout,
+    redirect: '/profile',
+    children: [
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/index.vue'),
+        meta: {
+          title: '个人中心',
+          icon: 'el-icon-user'
+        }
+      }
+    ]
   }
 ]
 const privateRoutes = [
@@ -16,10 +28,92 @@ const privateRoutes = [
     component: layout,
     redirect: '/user/manage',
     meta: {
-      title: 'user',
-      icon: 'personnel'
+      title: '用户',
+      icon: 'User'
     },
-    children: []
+    children: [
+      {
+        path: '/user/manage',
+        component: () => import('@/views/user-manage/index'),
+        meta: {
+          title: '用户管理',
+          icon: 'personnel-manage'
+        }
+      },
+      {
+        path: '/user/role',
+        component: () => import('@/views/role-list/index'),
+        meta: {
+          title: '角色列表',
+          icon: 'role'
+        }
+      },
+      {
+        path: '/user/permission',
+        component: () => import('@/views/permission-list/index'),
+        meta: {
+          title: '权限列表',
+          icon: 'permission'
+        }
+      },
+      {
+        path: '/user/info/:id',
+        name: 'userInfo',
+        component: () => import('@/views/user-info/index'),
+        meta: {
+          title: '用户信息'
+        }
+      },
+      {
+        path: '/user/import',
+        name: 'import',
+        component: () => import('@/views/import/index'),
+        meta: {
+          title: '表格导入'
+        }
+      }
+    ]
+  },
+  {
+    path: '/article',
+    component: layout,
+    redirect: '/article/ranking',
+    meta: {
+      title: '文章',
+      icon: 'article'
+    },
+    children: [
+      {
+        path: '/article/ranking',
+        component: () => import('@/views/article-ranking/index'),
+        meta: {
+          title: '文章排名',
+          icon: 'article-ranking'
+        }
+      },
+      {
+        path: '/article/:id',
+        component: () => import('@/views/article-detail/index'),
+        meta: {
+          title: '文章详情'
+        }
+      },
+      {
+        path: '/article/create',
+        component: () => import('@/views/article-create/index'),
+        meta: {
+          title: '创建文章',
+          icon: 'article-create'
+        }
+      },
+      {
+        path: '/article/editor/:id',
+        component: () => import('@/views/article-create/index'),
+        meta: {
+          title: '编辑文章'
+        }
+      }
+    ]
   }
 ]
 
